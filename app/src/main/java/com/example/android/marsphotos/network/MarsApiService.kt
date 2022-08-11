bfd81a54a6fbe960fcc051f247687feac98491d9
Sourabh Kumar <20010001030@gateway.edu.in>
2022-08-11 23:55:21 +0530

    Object declarations

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -12,10 +12,27 @@ private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .build()
 
+//initialize the retrofit service
+//this is the public singleton object that can be accessed from rest of app
+
+/**
+"lazy instantiation" is when object creation is purposely delayed until
+ you actually need that object to avoid unnecessary computation or use of
+other computing resources.
+*/
+
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        //initialize retrofit service
+        retrofit.create(MarsApiService::class.java)
+    }
+
+}
+
 //how Retrofit talks to the web server using HTTP requests
 interface MarsApiService {
     @GET("photos")
-    fun getPhotos()
+    fun getPhotos(): String
 }
 
 //class MarsApiService {
