c712d0cf91beb2084c06a07270d2baeee8b98c9e
Sourabh Kumar <20010001030@gateway.edu.in>
2022-08-12 23:54:49 +0530

    @json notation

--- app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -1,8 +1,12 @@
 package com.example.android.marsphotos.network
 
+import com.squareup.moshi.Json
+
 //Moshi parse the JSON object into Kotlin object
 //moshi need to have kotlin data class to store parse result 👇
 
-class MarsPhoto {
-
-}
+data class MarsPhoto(
+    val id : String,
+    //as both json and kotlin format are not same so we are using @json
+    @Json(name = "img_src") val imgSrcUrl : String
+)
