54070826e3b48b65182966b22ac26fefb721bc74
Sourabh Kumar <20010001030@gateway.edu.in>
2022-08-12 23:36:06 +0530

    created data class to store parse result

---
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -0,0 +1,8 @@
+package com.example.android.marsphotos.network
+
+//Moshi parse the JSON object into Kotlin object
+//moshi need to have kotlin data class to store parse result 👇
+
+class MarsPhoto {
+
+}
