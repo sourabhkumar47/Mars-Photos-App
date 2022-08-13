c62707f1d1bb8c1420cadc2af00002de83cc0629
Sourabh Kumar <20010001030@gateway.edu.in>
2022-08-14 00:14:46 +0530

    Create binding adapter and use Coil

--- app/src/main/java/com/example/android/marsphotos/BindingAdaptors.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdaptors.kt
@@ -1,2 +1,15 @@
 package com.example.android.marsphotos
 
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import coil.load
+
+@BindingAdapter //tell data binding to execute this binding when viewItem has URL
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        //convert the URL string to a Uri
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        imgView.load(imgUri)
+    }
+}
