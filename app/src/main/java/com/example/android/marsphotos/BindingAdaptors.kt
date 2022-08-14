a1b484b00a3385a8fe1249b689cc2eaebb085a7a
Sourabh Kumar <20010001030@gateway.edu.in>
2022-08-15 00:28:21 +0530

    added missing line in binding adaptor

--- app/src/main/java/com/example/android/marsphotos/BindingAdaptors.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdaptors.kt
@@ -5,7 +5,7 @@ import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import coil.load
 
-@BindingAdapter //tell data binding to execute this binding when viewItem has URL
+@BindingAdapter("imageUrl") //tell data binding to execute this binding when viewItem has URL
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
         //convert the URL string to a Uri
