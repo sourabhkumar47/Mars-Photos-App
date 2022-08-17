5bd5c31474e2885bb25afabf04d273887fbb7f80
Sourabh Kumar <20010001030@gateway.edu.in>
2022-08-18 00:35:15 +0530

    added binding recycler view

--- app/src/main/java/com/example/android/marsphotos/BindingAdaptors.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdaptors.kt
@@ -3,7 +3,10 @@ package com.example.android.marsphotos
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.PhotoGridAdapter
 
 @BindingAdapter("imageUrl") //tell data binding to execute this binding when viewItem has URL
 fun bindImage(imgView: ImageView, imgUrl: String?) {
@@ -16,4 +19,16 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
             error(R.drawable.ic_broken_image)
         }
     }
+}
+
+// initialize the PhotoGridAdapter with the list of MarsPhoto objects.
+//set the RecyclerView data causes data binding to automatically
+// observe the LiveData for the list of MarsPhoto objects
+@BindingAdapter("listData")
+fun bindRecycleView(
+    recyclerView: RecyclerView,
+    data: List<MarsPhoto>?
+) {
+    val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.submitList(data)
 }
