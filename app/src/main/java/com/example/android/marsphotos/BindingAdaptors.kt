b92ebd0669ef19a49e4d0db79054adec746e57ad
Sourabh Kumar <20010001030@gateway.edu.in>
2022-08-19 00:47:30 +0530

    Add binding adapter for the status ImageView

--- app/src/main/java/com/example/android/marsphotos/BindingAdaptors.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdaptors.kt
@@ -1,11 +1,13 @@
 package com.example.android.marsphotos
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.MarsApiStatus
 import com.example.android.marsphotos.overview.PhotoGridAdapter
 
 @BindingAdapter("imageUrl") //tell data binding to execute this binding when viewItem has URL
@@ -31,4 +33,25 @@ fun bindRecycleView(
 ) {
     val adapter = recyclerView.adapter as PhotoGridAdapter
     adapter.submitList(data)
+}
+
+//for the status ImageView
+@BindingAdapter("marsApiStatus")
+fun bindStatus(
+    statusImageView: ImageView,
+    status: MarsApiStatus?
+) {
+    when (status) {
+        MarsApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
 }
